35a48b4298391f2ba632522abbcfdd26c9dd5e90
sham <sham@csgo.com>
2025-07-31 14:56:40 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VisibleEventDirective } from './directive/visible-event.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VisibleEventDirective
   ],
   imports: [
     BrowserModule,
